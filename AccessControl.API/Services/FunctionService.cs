5603cd2f8f67ce8ee9882e6d470c364aeb39865d
Dhanglerson Barral <dhanglerson.dev@outlook.com>
2024-10-22 18:59:11 -0300

    update methodts

---
+++ AccessControl.API/Services/FunctionService.cs
@@ -0,0 +1,35 @@
+﻿using AccessControl.API.Data;
+using AccessControl.Core.Interfaces;
+using AccessControl.Core.Models;
+using AccessControl.Core.Requests;
+using AccessControl.Core.Responses;
+
+namespace AccessControl.API.Services;
+
+public class FunctionService(AppDbContext context) : IFunctionService
+{
+    public async Task<Function?> CreateFunctionAsync(Function function)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<PagedResponse<IEnumerable<Function>>> GetAllFunctionsAsync(PagedRequest pagedRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Function?> GetFunctionByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Function?> UpdateFunctionAsync(Function function)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Function?> DeleteFunctionAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
